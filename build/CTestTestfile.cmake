a1ccdb876a86e9c2b6aae4eae15af1c495a9b816
Fath16 <cffath16@gmail.com>
2024-04-23 20:46:25 -0400

    1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/theanvin/catkin_ws/src
+# Build directory: /home/theanvin/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("smb_gazebo")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/theanvin/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/theanvin/smb_ws_hw4/devel;/home/theanvin/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/theanvin/smb_ws_hw4/devel;/home/theanvin/catkin_ws/devel;/opt/ros/noetic' != '' else []
